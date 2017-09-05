2f090147dd536fda6d50185f1c3907ed7b3064a6
3k medialab <3kmedialab@gmail.com>
2017-09-05 21:44:58 +0200

    MIDI Controller class , config file and sketch

--- MidiController.ino
+++ MidiController.ino
@@ -1,83 +1,111 @@
-/*----------------------------------------------------------------------*
- * Example sketch for Arduino Button Library by Jack Christensen        *
- *                                                                      *
- * The simplest example. Using a tactile button switch to turn          *
- * the Arduino's pin 13 LED on and off. Wire a switch from Arduino      *
- * pin 2 to ground.                                                     *
- *                                                                      *
- * This work is licensed under the Creative Commons Attribution-        *
- * ShareAlike 3.0 Unported License. To view a copy of this license,     *
- * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
- * letter to Creative Commons, 171 Second Street, Suite 300,            *
- * San Francisco, California, 94105, USA.                               *
- *----------------------------------------------------------------------*/
+/*
+ * MIDI Controller
+ *
+ * Create the components that will be part of the Controller.
+ * Process the components of the controller.
+ *
+ * Created by Daniel Segura, 01/09/2017
+ */
 
- #include <MIDIController.h>
+#include <Pitches.h>
+#include <ControllerConfig.h>
+#include <MIDIController.h>
   
- #define BUTTON_PIN1 2
- #define BUTTON_PIN2 3
- #define BUTTON_PIN3 4   //Connect a tactile button switch (or something similar)
-                            //from Arduino pin 2 to ground.
- #define PULLUP true        //To keep things simple, we use the Arduino's internal pullup resistor.
- #define INVERT true        //Since the pullup resistor will keep the pin high unless the
-                            //switch is closed, this is negative logic, i.e. a high state
-                            //means the button is NOT pressed. (Assuming a normally open switch.)
- #define DEBOUNCE_MS 20     //A debounce time of 20 milliseconds usually works well for tactile button switches.
- #define LED_PIN 13         //The standard Arduino "Pin 13" LED
- 
- MIDI_CREATE_DEFAULT_INSTANCE();
- 
- Button myBtn1(BUTTON_PIN1, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
- Button myBtn2(BUTTON_PIN2, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
- 
- MIDIMessage noteOnOffMsgs1[2] ={ {midi::NoteOn, NOTE_A4, 127, 1}, {midi::NoteOff, NOTE_A4, 127, 1} };
- MIDIMessage shiftMessages1[2] ={ {midi::NoteOn, NOTE_C3, 127, 1}, {midi::NoteOff, NOTE_C3, 127, 1} };
- MIDIButton midiButton(BUTTON_PIN3, PULLUP, INVERT, DEBOUNCE_MS, noteOnOffMsgs1, shiftMessages1);    //Declare the button
- 
- MIDIMessage ccMessage(midi::ControlChange, midi::BreathController, 0, 1);
- MIDIPotentiometer midiPot(A0, &ccMessage);
- uint16_t controlValue = 0;
+// Create the MIDI interface object
+MidiInterface MIDI(Serial);
 
- Potentiometer myPot(A0);
- 
- boolean ledState;          //A variable that keeps the current LED status
+//-------------------------------- B U T T O N S  S E C T I O N ---------------------------------------------
+//Shift BUTTON
+Button shiftButton (BUTTON_PIN1, PULLUP, INVERT, DEBOUNCE_MS);
+
+//-------------------------------- E N D  B U T T O N S  S E C T I O N ---------------------------------------------
+
+
+//-------------------------------- M I D I  B U T T O N S  S E C T I O N ---------------------------------------------
+// MIDI BUTTON 1 MESSAGES
+MIDIMessage onPressedMessageB1(midi::NoteOn, NOTE_C3, 127, 1);
+MIDIMessage onReleasedMessageB1(midi::NoteOff, NOTE_C3, 127, 1);
+
+// MIDI BUTTON 2 MESSAGES
+MIDIMessage onPressedMessageB2(midi::NoteOn, NOTE_F3, 127, 1);
+MIDIMessage onReleasedMessageB2(midi::NoteOff, NOTE_F3, 127, 1);
+
+// MIDI BUTTON 3 MESSAGES
+MIDIMessage onPressedMessageB3(midi::NoteOn, NOTE_G3, 127, 1);
+MIDIMessage onReleasedMessageB3(midi::NoteOff, NOTE_G3, 127, 1);
+
+// MIDI BUTTONS
+MIDIButton midiButtons [NUM_MIDI_BUTTONS] = { 
+    {MIDI_BUTTON_PIN1, PULLUP, INVERT, DEBOUNCE_MS, &onPressedMessageB1, &onReleasedMessageB1},
+    {MIDI_BUTTON_PIN2, PULLUP, INVERT, DEBOUNCE_MS, &onPressedMessageB2, &onReleasedMessageB2},
+    {MIDI_BUTTON_PIN3, PULLUP, INVERT, DEBOUNCE_MS, &onPressedMessageB3, &onReleasedMessageB3}    
+};
+
+// MIDI BUTTON 1 SHIFT MESSAGES
+MIDIMessage onPressedShiftMessageB1(midi::NoteOn, NOTE_C4, 127, 1);
+MIDIMessage onReleasedShiftMessageB1(-1, -1, -1, -1);
+
+// MIDI BUTTON 2 SHIFT MESSAGES
+MIDIMessage onPressedShiftMessageB2(midi::NoteOn, NOTE_F4, 127, 1);
+MIDIMessage onReleasedShiftMessageB2(-1, -1, -1, -1);
+
+// MIDI BUTTON 3 SHIFT MESSAGES
+MIDIMessage onPressedShiftMessageB3(midi::ControlChange, midi::Sustain, 127, 1);
+MIDIMessage onReleasedShiftMessageB3(midi::ControlChange, midi::Sustain, 0, 1);
+
+// MIDI SHIFT BUTTONS
+MIDIButton shiftMidiButtons [NUM_MIDI_BUTTONS] = { 
+    {MIDI_BUTTON_PIN1, PULLUP, INVERT, DEBOUNCE_MS, &onPressedShiftMessageB1, &onReleasedShiftMessageB1},
+    {MIDI_BUTTON_PIN2, PULLUP, INVERT, DEBOUNCE_MS, &onPressedShiftMessageB2, &onReleasedShiftMessageB2},
+    {MIDI_BUTTON_PIN3, PULLUP, INVERT, DEBOUNCE_MS, &onPressedShiftMessageB3, &onReleasedShiftMessageB3}    
+};
+
+
+//-------------------------------- E N D  M I D I  B U T T O N S  S E C T I O N ---------------------------------------------
+
+//-------------------------------- M I D I  P O T E N T I O M E T E R S  S E C T I O N ---------------------------------------------
+// MIDI POTENTIOMETER 1 MESSAGE
+MIDIMessage midiMessageP1(midi::ControlChange, midi::BreathController, 0, 1);
+
+// MIDI POTENTIOMETERS
+MIDIPotentiometer midiPots [NUM_MIDI_POTS] = {
+    {MIDI_POT_PIN1, WINDOW_SIZE, &midiMessageP1}
+};
+
+// MIDI POTENTIOMETER 1 SHIFT MESSAGE
+MIDIMessage midiShiftMessageP1(midi::ControlChange, midi::BreathController, 0, 1);
+
+// MIDI SHIFT POTENTIOMETERS
+MIDIPotentiometer shiftMidiPots [NUM_MIDI_POTS] = {
+    {MIDI_POT_PIN1, WINDOW_SIZE, &midiShiftMessageP1}
+};
+//-------------------------------- E N D  M I D I  P O T E N T I O M E T E R S  S E C T I O N ---------------------------------------------
+
+// Creates the MIDI Controller object
+MIDIController controller(MIDI, &shiftButton, midiButtons, midiPots, NUM_MIDI_BUTTONS, NUM_MIDI_POTS);
  
  void setup(void)
  {
-    MIDI.begin(); 
-    pinMode(LED_PIN, OUTPUT);    //Set the LED pin as an output
+    controller.begin();      
  }
  
  void loop(void)
  {
-     myBtn1.read();                    //Read the button
- 
-     if (myBtn1.wasReleased()) {       //If the button was released, change the LED state
-         ledState = !ledState;
-         digitalWrite(LED_PIN, ledState);       
-     }
- 
-     myBtn2.read();                    //Read the button
- 
-     if (myBtn2.wasReleased()) {       //If the button was released, change the LED state
-         ledState = !ledState;
-         digitalWrite(LED_PIN, ledState);        
-     }
- 
-     midiButton.read();                    //Read the button
- 
-     if (midiButton.wasPressed()) {       //If the button was released, change the LED state
-       MIDI.sendNoteOn(midiButton.getMessage(0).getDataByte1(), midiButton.getMessage(0).getDataByte2(), midiButton.getMessage(0).getChannel() );
-     }    
-     
-     if (midiButton.wasReleased()) {       //If the button was released, change the LED state
-       MIDI.sendNoteOff(midiButton.getMessage(1).getDataByte1(), midiButton.getMessage(1).getDataByte2(), midiButton.getMessage(1).getChannel() );
-     }
- 
-     // PROCESS KNOBS
-     controlValue = midiPot.getSmoothValue();
- 
-     if (midiPot.wasChanged()) {
-          MIDI.sendControlChange(midiPot.getMessage().getDataByte1(), map(controlValue,0,1023,0,127), midiPot.getMessage().getChannel());
+     controller.processMIDIButtons();
+     controller.processMIDIPots();    
+     if (controller.wasShiftButtonReleased())
+     {
+        if (!controller.isShiftMode())
+        {
+            controller.setMIDIButtons(shiftMidiButtons);
+            controller.setMIDIPotentiometers(shiftMidiPots);
+        }
+        else
+        {
+            controller.setMIDIButtons(midiButtons);
+            controller.setMIDIPotentiometers(midiPots);
+        }
+
+        controller.setShiftMode(!controller.isShiftMode());
      }
  }
